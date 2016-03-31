e64f4336950578870141cf1a22cea1a085676c2d
Stefan Dellmuth <stefan.dellmuth@msg-systems.com>
2016-03-31 15:01:40 +0200

    Restliche, fehlschlagende Testfälle korrigiert
    Resolves: #10
    See: #11

---
+++ src/test/java/de/msg/terminfindung/persistence/dao/TeilnehmerZeitraumDaoTest.java
@@ -0,0 +1,89 @@
+package de.msg.terminfindung.persistence.dao;
+
+/*
+ * #%L
+ * Terminfindung
+ * %%
+ * Copyright (C) 2015 - 2016 Bundesverwaltungsamt (BVA), msg systems ag
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+import de.msg.terminfindung.persistence.entity.Praeferenz;
+import de.msg.terminfindung.persistence.entity.Teilnehmer;
+import de.msg.terminfindung.persistence.entity.TeilnehmerZeitraum;
+import de.msg.terminfindung.persistence.entity.Zeitraum;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+public class TeilnehmerZeitraumDaoTest extends AbstraktDaoTest {
+
+    private static final Long TEILNEHMER_NICHT_ZUGEORDNET = 3L;
+
+    private static final Long ZEITRAUM_MORGENS = 1L;
+
+    private static final Long ZEITRAUM_MITTAGS = 2L;
+
+    private static final Long TEILNEHMER_ZEITRAUM_ID = 4L;
+
+    @Autowired
+    private TeilnehmerDao teilnehmerDao;
+
+    @Autowired
+    private ZeitraumDao zeitraumDao;
+
+    @Autowired
+    private TeilnehmerZeitraumDao tzDao;
+
+    @Test
+    @DatabaseSetup("testTeilnehmerZeitraumDaoSetup.xml")
+    @ExpectedDatabase(value = "testTeilnehmerZeitraumDaoSpeichern.xml", assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED)
+    public void testSpeichern() {
+        Teilnehmer teilnehmer = teilnehmerDao.sucheMitId(TEILNEHMER_NICHT_ZUGEORDNET);
+
+        Zeitraum morgens = zeitraumDao.sucheMitId(ZEITRAUM_MORGENS);
+        Zeitraum mittags = zeitraumDao.sucheMitId(ZEITRAUM_MITTAGS);
+
+        tzDao.speichere(new TeilnehmerZeitraum(teilnehmer, morgens, Praeferenz.JA));
+        tzDao.speichere(new TeilnehmerZeitraum(teilnehmer, mittags, Praeferenz.WENN_ES_SEIN_MUSS));
+    }
+
+    @Test
+    @DatabaseSetup("testTeilnehmerZeitraumDaoSetup.xml")
+    public void testSuchenMitId() {
+        TeilnehmerZeitraum teilnehmerZeitraum = tzDao.sucheMitId(TEILNEHMER_ZEITRAUM_ID);
+
+        assertNotNull(teilnehmerZeitraum);
+        assertEquals(2, teilnehmerZeitraum.getTeilnehmer().getTeilnehmer_Nr());
+        assertEquals(ZEITRAUM_MITTAGS, Long.valueOf(teilnehmerZeitraum.getZeitraum().getZeitraum_Nr()));
+        assertEquals(Praeferenz.JA, teilnehmerZeitraum.getPraeferenz());
+    }
+
+    @Test
+    @DatabaseSetup("testTeilnehmerZeitraumDaoSetup.xml")
+    @ExpectedDatabase(value = "testTeilnehmerZeitraumDaoLoeschen.xml", assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED)
+    public void testLoesche() {
+        TeilnehmerZeitraum teilnehmerZeitraum = tzDao.sucheMitId(TEILNEHMER_ZEITRAUM_ID);
+        tzDao.loesche(teilnehmerZeitraum);
+
+        assertNull(tzDao.sucheMitId(TEILNEHMER_ZEITRAUM_ID));
+    }
+
+}
---
+++ src/test/java/de/msg/terminfindung/persistence/dao/TerminfindungDaoTest.java
@@ -0,0 +1,69 @@
+package de.msg.terminfindung.persistence.dao;
+
+/*
+ * #%L
+ * Terminfindung
+ * %%
+ * Copyright (C) 2015 - 2016 Bundesverwaltungsamt (BVA), msg systems ag
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+import de.msg.terminfindung.persistence.entity.Organisator;
+import de.msg.terminfindung.persistence.entity.Terminfindung;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+public class TerminfindungDaoTest extends AbstraktDaoTest {
+
+    private static final Long TERMINFINDUNG_ID = 2L;
+
+    @Autowired
+    private TerminfindungDao terminfindungDao;
+
+    @Test
+    @DatabaseSetup("testTerminfindungDaoSetup.xml")
+    @ExpectedDatabase(value = "testTerminfindungDaoSpeichern.xml", assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED)
+    public void testSpeichern() {
+        terminfindungDao.speichere(new Terminfindung(new Organisator("Sepp"), "Grillfest 2016"));
+    }
+
+    @Test
+    @DatabaseSetup("testTerminfindungDaoSetup.xml")
+    public void testSuchenMitId() {
+        Terminfindung terminfindung = terminfindungDao.sucheMitId(TERMINFINDUNG_ID);
+
+        assertNotNull(terminfindung);
+        assertEquals("Herbert", terminfindung.getOrganisator().getName());
+        assertEquals("Spieleabend IsyFact", terminfindung.getVeranstName());
+    }
+
+    @Test
+    @DatabaseSetup("testTerminfindungDaoSetup.xml")
+    @ExpectedDatabase(value = "testTerminfindungDaoLoeschen.xml", assertionMode = DatabaseAssertionMode.NON_STRICT_UNORDERED)
+    public void testLoesche() {
+        Terminfindung terminfindung = terminfindungDao.sucheMitId(TERMINFINDUNG_ID);
+        terminfindungDao.loesche(terminfindung);
+
+        assertNull(terminfindungDao.sucheMitId(TERMINFINDUNG_ID));
+    }
+
+
+}
