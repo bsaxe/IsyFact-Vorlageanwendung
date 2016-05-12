2bfc749d78ee12ac1c3b2e3a5c41bd70dec1dd4d
Stefan Dellmuth <stefan.dellmuth@msg-systems.com>
2016-05-12 15:45:32 +0200

    Selbsttest und JMX-Überwachung konfiguriert
    Der Selbsttest der Anwendung prüft die Verfügbarkeit der Nachbarsysteme, d.h. der Datenbank.
    Die JMX-Überwachung stellt das Ergebnis des Selbsttests und Statistiken über die GUI-Controller bereit.
    Die Konfiguration wird nun jede Minute neu eingelesen und auf Änderungen überprüft.
    
    Siehe: #4

---
+++ src/main/java/de/msg/terminfindung/gui/administration/TerminfindungWatchdog.java
@@ -0,0 +1,40 @@
+package de.msg.terminfindung.gui.administration;
+
+import de.bund.bva.pliscommon.konfiguration.common.Konfiguration;
+import de.bund.bva.pliscommon.ueberwachung.admin.impl.WatchdogImpl;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.persistence.EntityManager;
+import java.util.concurrent.Callable;
+
+public class TerminfindungWatchdog extends WatchdogImpl implements InitializingBean {
+
+    /**
+     * SQL-Query, welche der Watchdog zur Prüfung der Datenbankverbindung ausführt.
+     */
+    private static final String CONF_ADMIN_WATCHDOG_VALIDATION_QUERY = "admin.watchdog.validation.query";
+
+    private Konfiguration konfiguration;
+
+    private EntityManager entityManager;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        addPruefung("Datenbank", new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                final String watchdogQuery = konfiguration.getAsString(CONF_ADMIN_WATCHDOG_VALIDATION_QUERY);
+                entityManager.createNativeQuery(watchdogQuery).getSingleResult();
+                return true;
+            }
+        });
+    }
+
+    public void setKonfiguration(Konfiguration konfiguration) {
+        this.konfiguration = konfiguration;
+    }
+
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+}
