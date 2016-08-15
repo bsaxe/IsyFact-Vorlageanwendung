85c16246dfdca5118158aaee26881cc11151ecea
bsaxe <bjoernsaxe@gmx.de>
2016-08-15 10:49:03 +0200

    Lizenzheader ergänzt
    - Apache Lizenz Header ergänzt
    - Test-URL auf locahost geändert
    - Formatierung pom.xml korrigiert

--- src/test/java/de/msg/terminfindung/gui/pages/TeilnahmeTabelle.java
+++ src/test/java/de/msg/terminfindung/gui/pages/TeilnahmeTabelle.java
@@ -1,5 +1,25 @@
 package de.msg.terminfindung.gui.pages;
 
+/*
+ * #%L
+ * Terminfindung
+ * %%
+ * Copyright (C) 2015 - 2016 Bundesverwaltungsamt (BVA), msg systems ag
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -103,7 +123,7 @@ class TeilnahmeTabelle {
 					columnSpans.add(Integer.parseInt(column.attr("colspan")));
 					tage.add(LocalDate.parse(column.text(), ddMMyyyy));
 				}
-				// Zeiträume ohne colspan
+				// Zeitrï¿½ume ohne colspan
 				else if (column.attr("class").startsWith("borderTd ") && column.attr("colspan").isEmpty()) {
 					zeitraeume.add(column.text());
 				}
@@ -207,7 +227,7 @@ class TeilnahmeTabelle {
 		}
 	}
 
-	// Zuordnung der Spaltenindizes der Zeitraumspalten auf deren zugehörigen
+	// Zuordnung der Spaltenindizes der Zeitraumspalten auf deren zugehï¿½rigen
 	// Tag
 	private LocalDate getTagFuerIndex(int idx) {
 		int tagIdx = 0;
--- src/test/java/de/msg/terminfindung/gui/pages/fragments/TerminfindungDetailsFragment.java
+++ src/test/java/de/msg/terminfindung/gui/pages/fragments/TerminfindungDetailsFragment.java
@@ -1,5 +1,25 @@
 package de.msg.terminfindung.gui.pages.fragments;
 
+/*
+ * #%L
+ * Terminfindung
+ * %%
+ * Copyright (C) 2015 - 2016 Bundesverwaltungsamt (BVA), msg systems ag
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.jboss.arquillian.graphene.fragment.Root;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
